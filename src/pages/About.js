c8d7e9fb562910a36e075810ae64e2009e35bd9d
Todd James <tjames@Todds-MacBook-Pro.local>
2020-12-27 11:29:45 -0500

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae,
+        quaerat ullam. Libero, asperiores in repellat voluptate, odit placeat,
+        officia nesciunt est quidem veritatis dolorum laborum voluptatibus
+        minima architecto nam quam! Odit suscipit voluptate facere pariatur
+        iusto alias, qui expedita mollitia.
+      </p>
+    </section>
+  );
+};
+
+export default About;
